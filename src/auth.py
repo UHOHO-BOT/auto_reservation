093d49a09aea69d4085d79cbc99c7d5ee0ad15e2
uhoho <ereser555@gmail.com>
2025-03-27 23:17:13 +0900

    auth.py 関数化

--- src/auth.py
+++ src/auth.py
@@ -29,29 +29,19 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-# ChromeDriverのパスを設定
-driver_path = r'C:\Users\sakuras\pystudy\auto_reservation\src\chromedriver-win64\chromedriver-win64\chromedriver.exe'
+from page_navigation import click_target_element
 
-# Serviceオブジェクトを作成
-service = Service(driver_path)
+def passlogin(PASS_PATH): 
+    # auth.txtからIDとパスワードを読み込む
+    with open(PASS_PATH, 'r') as file:
+        lines = file.readlines()
+        hamaid = lines[0].strip()
+        hamapass = lines[1].strip()
+    return hamaid,hamapass
 
-# WebDriverを初期化
-driver = webdriver.Chrome(service=service)
-
-
-# auth.txtからIDとパスワードを読み込む
-with open('passid.txt', 'r') as file:
-    lines = file.readlines()
-    hamaid = lines[0].strip()
-    hamapass = lines[1].strip()
-
-# ブラウザを開く
-driver = webdriver.Chrome(service=service)
-
-
-try:
+def login (LOGIN_PAGE_PATH,hamaid,hamapass):
      # 手順1: ログインページにアクセス
-    driver.get('https://www.shisetsu.city.yokohama.lg.jp/user/Login')
+    driver.get(LOGIN_PAGE_PATH)
 
     # 手順2, 3: フォームにIDとパスワードを入力
     id_field = WebDriverWait(driver, 10).until(
@@ -63,15 +53,33 @@ try:
     pass_field.send_keys(hamapass)
 
     # 手順4: ログインボタンを押す
-    login_button = WebDriverWait(driver, 10).until(
-        EC.element_to_be_clickable((By.XPATH, '//*[@id="app"]/form/div[2]/ul/li[1]/button'))
-    )
-    login_button.click()
 
-    time.sleep(10)
+    locator = '//*[@id="app"]/form/div[2]/ul/li[1]/button'
+
+    click_target_element(driver, locator, timeout=10)
+
+   # login_button = WebDriverWait(driver, 10).until(
+   #     EC.element_to_be_clickable((By.XPATH, '//*[@id="app"]/form/div[2]/ul/li[1]/button'))
+   # )
+   # login_button.click()
+
+    #time.sleep(10)
+# ページを開いたままにする
+    input("Press Enter to close the browser and exit...")
 
 
-
-finally:
+#finally:
     # Do nothing to keep the browser open
-    pass
+#    pass
+
+
+# テスト用のコード
+if __name__ == "__main__":
+    # テスト用データや設定
+    PASS_PATH = '.\passid.txt'
+    LOGIN_PAGE_PATH = 'https://www.shisetsu.city.yokohama.lg.jp/user/Login'
+
+    # 関数のテスト実行
+    hamapass,hamaid=passlogin(PASS_PATH)
+    login(LOGIN_PAGE_PATH,hamaid,hamapass)
+
